bc6983d7cc1156b1a111fa9c95486d82fc3b1942
Katherine Trammell <ktrammell@gmail.com>
2015-03-02 14:12:35 -0500

    passed is polymorphic test

--- js/homework.js
+++ js/homework.js
@@ -1,16 +1,47 @@
 function Vehicle() {
-  this.notLiving=true;
-}
+  this.notLiving = true;
+};
 
-function Car() {
-  Vehicle.call(this);
-}
+  function LandVehicle() {
+    Vehicle.call(this);
+  };
+  //this makes it instance of vehicle
+    LandVehicle.prototype = Object.create(Vehicle.prototype);
 
-//this makes it instance of vehicle
-Car.prototype = Object.create(Vehicle.prototype));
+      function Car() {
+        LandVehicle.call(this);
+      };
 
-function Aircraft(){
-  Vehicle.call(this);
-}
+      Car.prototype = Object.create(LandVehicle.prototype);
+
+      function Train() {
+        LandVehicle.call(this);
+      };
+
+      Train.prototype = Object.create(LandVehicle.prototype);
 
 
+  function Aircraft() {
+    Vehicle.call(this);
+  };
+
+    Aircraft.prototype = Object.create(Vehicle.prototype);
+
+      function Plane() {
+        Aircraft.call(this);
+      };
+
+      Plane.prototype = Object.create(Aircraft.prototype);
+
+
+  function SeaVessel() {
+    Vehicle.call(this);
+   };
+
+    SeaVessel.prototype = Object.create(Vehicle.prototype);
+
+      function Boat() {
+        SeaVessel.call(this);
+      };
+
+      Boat.prototype = Object.create(SeaVessel.prototype);
